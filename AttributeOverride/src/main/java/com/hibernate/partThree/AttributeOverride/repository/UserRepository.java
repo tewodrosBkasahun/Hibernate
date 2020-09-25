60895f750636d34e2b29f5c2706d7c7247507f89
Tewodros Kasahun <tewodros.b.kasahun@gmail.com>
2020-09-25 12:17:11 -0400

    Added @Repository and @Transactional

--- AttributeOverride/src/main/java/com/hibernate/partThree/AttributeOverride/repository/UserRepository.java
+++ AttributeOverride/src/main/java/com/hibernate/partThree/AttributeOverride/repository/UserRepository.java
@@ -2,9 +2,14 @@ package com.hibernate.partThree.AttributeOverride.repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
 
 import com.hibernate.partThree.AttributeOverride.entity.Person;
 
+@Repository
+@Transactional
 public class UserRepository {
 
 	@PersistenceContext
